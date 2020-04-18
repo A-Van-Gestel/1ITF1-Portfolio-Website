13b6eb189f454203d99c06a4041ee7028d312e7e
Axel Van Gestel <r0784084@student.thomasmore.be>
2020-04-18 16:31:07 +0200

    Updated navbar (Transparency effect update)

--- js/nav-transparant-to-solid.js
+++ js/nav-transparant-to-solid.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     // Transition effect for navbar
     $(window).scroll(function() {
         // checks if window is scrolled more than 500px, adds/removes solid class
-        if($(this).scrollTop() > 184) {
-            $('.navbar').addClass('bg-white');
+        if($(this).scrollTop() > 232) {
+            $('.navbar').addClass('bg-white-blur');
             // $('.navbar').addClass('navbar-dark');
             $('.navbar').removeClass('bg-transparent');
             // $('.navbar').removeClass('navbar-light');
         } else {
-            $('.navbar').removeClass('bg-white');
+            $('.navbar').removeClass('bg-white-blur');
             // $('.navbar').removeClass('navbar-dark');
             $('.navbar').addClass('bg-transparent');
             // $('.navbar').addClass('navbar-light');
