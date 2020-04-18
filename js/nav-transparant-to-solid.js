f54cc191095b3ef1c5c3f330c38902e888da7c9a
Axel Van Gestel <r0784084@student.thomasmore.be>
2020-04-18 16:56:25 +0200

    Updated navbar (blur now defaulth)

--- js/nav-transparant-to-solid.js
+++ js/nav-transparant-to-solid.js
@@ -3,15 +3,11 @@ $(document).ready(function() {
     $(window).scroll(function() {
         // checks if window is scrolled more than 500px, adds/removes solid class
         if($(this).scrollTop() > 232) {
-            $('.navbar').addClass('bg-white-blur');
-            // $('.navbar').addClass('navbar-dark');
+            // $('.navbar').addClass('bg-white-blur');
             $('.navbar').removeClass('bg-transparent');
-            // $('.navbar').removeClass('navbar-light');
         } else {
-            $('.navbar').removeClass('bg-white-blur');
-            // $('.navbar').removeClass('navbar-dark');
+            // $('.navbar').removeClass('bg-white-blur');
             $('.navbar').addClass('bg-transparent');
-            // $('.navbar').addClass('navbar-light');
         }
     });
 });
